d4976ab0aa6e1ef454de7fa2190525016ef2db1c
atreion <5829476+atreion@users.noreply.github.com>
2025-04-16 10:40:29 +0200

    fix: update bufferline keymaps

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,1 +1,26 @@
-return { 'akinsho/bufferline.nvim', version = '*', dependencies = 'nvim-tree/nvim-web-devicons', opts = {} }
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {},
+  -- keys = {
+  --   { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+  --   { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+  --   { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+  --   { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+  --   { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+  --   { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+  --   { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+  --   { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+  --   { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+  --   { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+  -- },
+  config = function()
+    require('bufferline').setup()
+    vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Buffer' })
+    vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<cr>', { desc = 'Next Buffer' })
+    -- vim.opt.termguicolors = true
+    -- vim.opt.showtabline = 2
+    -- vim.opt.tabline = '%!v:lua.require("bufferline").get_tabline()'
+  end,
+}
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -55,14 +55,14 @@ return {
       desc = 'Debug: Step Out',
     },
     {
-      '<leader>b',
+      '<leader>tb',
       function()
         require('dap').toggle_breakpoint()
       end,
       desc = 'Debug: Toggle Breakpoint',
     },
     {
-      '<leader>B',
+      '<leader>tB',
       function()
         require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
       end,
